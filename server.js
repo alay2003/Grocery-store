992d648aa3eb6b5b671f09168905643eaabfdeb6
Alay Patel <alaypatel1920@gmail.com>
2024-10-17 20:43:48 +0530

    docker file update

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.post('/checkout', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:3000');
 });
+

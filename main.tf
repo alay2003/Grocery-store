258ecee5ea6e2e58e84b71de957a8aa85b60a0d2
Alay Patel <alaypatel1920@gmail.com>
2024-10-18 10:31:10 +0530

    jenkinsminikube code updated

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Point to your kubeconfig file
+}
+
+resource "kubernetes_deployment" "grocery_store" {
+  metadata {
+    name = "grocery-store"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "grocery-store"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "grocery-store"
+        }
+      }
+
+      spec {
+        container {
+          image = "alay2003/grocery-store:alay"
+          name  = "grocery-store"
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "grocery_store_service" {
+  metadata {
+    name = "grocery-store-service"
+  }
+
+  spec {
+    selector = {
+      app = "grocery-store"
+    }
+
+    port {
+      port        = 80
+      target_port = 3000
+    }
+
+    type = "NodePort"
+  }
+}

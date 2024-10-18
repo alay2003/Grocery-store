065c951869b9c1053ae890cfb1332bfcbed73bb1
dishankjain14 <dishankjain14@gmail.com>
2024-10-18 17:15:53 +0530

    a

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "kubernetes" {
-  config_path = "C:\\Users\\ibhad\\.kube\\config"  # Point to your kubeconfig file
+  config_path = "C:\\Users\\disha\\.kube\\config"  # Point to your kubeconfig file
+  
 }
 
 resource "kubernetes_deployment" "grocery_store" {
